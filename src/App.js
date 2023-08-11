7f658b5c719f9babba2e3a3d30babbeafaa81958
Wiktor Nazaruk <89415407+wiktornazaruk@users.noreply.github.com>
2023-08-11 18:53:55 +0200

    added simple responsiveness

--- src/App.js
+++ src/App.js
@@ -6,20 +6,40 @@ import Sidebar from "./components/Sidebar";
 import Month from "./components/Month";
 import GlobalContext from "./context/GlobalContext";
 import EventModel from "./components/EventModel";
+import { Dimensions } from "react-native";
+
+const windowDimensions = Dimensions.get("window");
+const screenDimensions = Dimensions.get("screen");
 
 function App() {
   const [currMonth, setCurrMonth] = useState(getMonth());
   const { monthIndex, showEventModel } = useContext(GlobalContext);
+  const [dimensions, setDimensions] = useState({
+    window: windowDimensions,
+    screen: screenDimensions,
+  });
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener(
+      "change",
+      ({ window, screen }) => {
+        setDimensions({ window, screen });
+      }
+    );
+    return () => subscription?.remove();
+  });
+
   useEffect(() => {
     setCurrMonth(getMonth(monthIndex));
   }, [monthIndex]);
+
   return (
     <React.Fragment>
       {showEventModel && <EventModel />}
       <div className="h-screen flex flex-col bg-slate-800 text-white">
         <CalendarHeader />
         <div className="flex flex-1">
-          <Sidebar />
+          {dimensions.window.width > 1000 && <Sidebar />}
           <Month month={currMonth} />
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   content: [],
   theme: {
+    minWidth: {
+      398: "398px",
+    },
+    minHeight: {
+      382: "382px",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
