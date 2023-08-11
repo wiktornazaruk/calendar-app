c86d15c848ebb633d790a540d3a8f05fc2ffbda4
Wiktor Nazaruk <89415407+wiktornazaruk@users.noreply.github.com>
2023-08-11 20:03:12 +0200

    changed responsivity

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,9 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import GlobalContext from "../context/GlobalContext";
 import dayjs from "dayjs";
+import { Dimensions } from "react-native";
+
+const windowDimensions = Dimensions.get("window");
+const screenDimensions = Dimensions.get("screen");
 
 export default function CalendarHeader() {
   const { monthIndex, setMonthIndex } = useContext(GlobalContext);
+  const [dimensions, setDimensions] = useState({
+    window: windowDimensions,
+    screen: screenDimensions,
+  });
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener(
+      "change",
+      ({ window, screen }) => {
+        setDimensions({ window, screen });
+      }
+    );
+    return () => subscription?.remove();
+  });
   function handlePrevMonth() {
     setMonthIndex(monthIndex - 1);
   }
@@ -17,27 +35,30 @@ export default function CalendarHeader() {
         : dayjs().month()
     );
   }
+  function setTextSize() {
+    return dimensions.window.width > 500 ? "text-xl" : "text-sm";
+  }
   return (
     <div className="border-x">
       <header className="px-4 py-2 flex items-center">
-        <h1 className="mr-10 text-xl font-bold">Calendar</h1>
+        <h1 className={`mr-4 ${setTextSize} font-bold`}>Calendar</h1>
         <button
           onClick={handleCurrMonth}
-          className="bg-green-500 text-white border-slate-700 border rounded-full py-2 px-4 mr-5"
+          className="bg-green-500 text-white border-slate-700 border rounded-full py-2 px-4 mr-3"
         >
           Today
         </button>
         <button onClick={handlePrevMonth}>
-          <span className="material-icons-outlined cursor-pointer mx-2 my-2">
+          <span className="material-icons-outlined cursor-pointer my-2">
             chevron_left
           </span>
         </button>
         <button onClick={handleNextMonth}>
-          <span className="material-icons-outlined cursor-pointer mx-2 my-2">
+          <span className="material-icons-outlined cursor-pointer mx-3 my-2">
             chevron_right
           </span>
         </button>
-        <h2 className="ml-4 text-xl font-bold">
+        <h2 className={`${setTextSize} font-bold`}>
           {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
         </h2>
       </header>
